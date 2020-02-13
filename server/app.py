c46b35d3c5f4a50d7b60fd92c864bb5e0eb5b8a6
Keenan Thomson <keenanthomson@gmail.com>
2020-02-13 09:36:59 -0500

    get server to respond with html

--- server/app.py
+++ server/app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def root():
+    return send_from_directory('../client/dist/', 'index.html')
 
 if __name__ == '__main__':
     app.run()
