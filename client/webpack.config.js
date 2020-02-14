a25f7c19a1a02f92f6aeab3f63469931d97d7417
Keenan Thomson <keenanthomson@gmail.com>
2020-02-13 23:20:38 -0500

    refactor flask server; add webpack bundle; react rendering correctly

---
+++ client/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, '/src');
+const DIST_DIR = path.join(__dirname, '/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    path: DIST_DIR,
+    filename: `bundle.js`
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        exclude: /(node_modules)/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/react']
+            }
+          }
+        ],
+      },
+      {
+        test: /\.css$/,
+        use:['style-loader', 'css-loader']
+      }
+    ]
+  }
+}
--- server/app.py
+++ server/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_folder='../client/dist')
 
 
-@app.route('/')
-def root():
-    return send_from_directory('../client/dist/', 'index.html')
+@app.route('/<path:filename>')
+def root(filename):
+    return send_from_directory('../client/dist/', filename)
 
 if __name__ == '__main__':
     app.run()
